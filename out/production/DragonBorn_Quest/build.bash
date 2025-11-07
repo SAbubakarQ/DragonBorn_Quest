cf9f1c8844213bc87800cf7b41fc2ab1e3ded911
Abubakar Qasim <sahibzadaabubakar.b.qasim@jbhunt.com>
2025-11-07 09:28:12 -0600

    Removed out folder

--- out/production/DragonBorn_Quest/build.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-clear
-set -u -e
-javac game_DragonBornQuest.service.Game.java View.java Controller.java Model.java Brick.java Json.java Mario.java Sprite.java Coin.java
-echo "Compiling completed! Running Game..."
-java game_DragonBornQuest.service.Game
-rm *.class
